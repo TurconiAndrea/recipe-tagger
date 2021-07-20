9e67391cadafef8f19b6803b930e587dcb9717ee
TurconiAndrea <darthsuper@gmail.com>
2021-07-20 11:06:59 +0200

    Upgrade version from 0.2.2 to 0.3.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 setup(
     name='recipe-tagger',
     packages=find_packages(include=['recipe_tagger']),
-    version='0.2.2',
+    version='0.3.2',
     description='A library for tagging and classify recipes',
     author='Andrea Turconi',
     license='MIT',
