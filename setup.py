c4687f3ea0a236aad85d02d5f033e9f62d0c906f
TurconiAndrea <darthsuper@gmail.com>
2021-09-27 20:01:31 +0200

    Update version from 0.4.8 to 0.4.9

--- setup.py
+++ setup.py
@@ -16,14 +16,14 @@ README = (HERE / "README.md").read_text()
 setup(
     name="recipe-tagger",
     packages=find_packages(include=["recipe_tagger"]),
-    version="0.4.8",
+    version="0.4.9",
     description="A library for tagging and classify recipes",
     author="Andrea Turconi",
     license="MIT",
     long_description=README,
     long_description_content_type="text/markdown",
     url="https://github.com/TurconiAndrea/recipe-tagger",
-    download_url="https://github.com/TurconiAndrea/recipe-tagger/archive/refs/tags/0.4.8.tar.gz",
+    download_url="https://github.com/TurconiAndrea/recipe-tagger/archive/refs/tags/0.4.9.tar.gz",
     keywords=["food", "recipe", "tag", "tagging", "ingredient", "water footprint"],
     install_requires=[
         "wikipedia-api",
